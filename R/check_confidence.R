51bd2dcba7a8c01f9397a37b30f0924a4d49b7c7
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2025-10-21 14:40:17 +0200

    Add median, min and max

--- R/check_confidence.R
+++ R/check_confidence.R
@@ -46,9 +46,11 @@ check_confidence.default <- function(
   summarised_data <- data |>
     dplyr::ungroup() |>
     dplyr::summarise(
-      mean_confidence = mean(.data$confidence, na.rm = TRUE),
-      q1 = stats::quantile(.data$confidence, probs = 0.25),
-      q3 = stats::quantile(.data$confidence, probs = 0.75),
+      median_conf = stats::median(.data$confidence, na.rm = TRUE),
+      q1_conf = stats::quantile(.data$confidence, probs = 0.25),
+      q3_conf = stats::quantile(.data$confidence, probs = 0.75),
+      min_conf = min(.data$confidence, na.rm = TRUE),
+      max_conf = max(.data$confidence, na.rm = TRUE),
       .by = by
     ) |>
     suppressWarnings()
