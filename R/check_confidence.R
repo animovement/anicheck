e03aa963f640ba3dc3cb1ac91e2dddeedb1a2798
Mikkel Roald-Arbøl <github.ggb9a@simplelogin.co>
2025-10-21 14:11:42 +0200

    Make one function and pkg docs

---
+++ R/check_confidence.R
@@ -0,0 +1,100 @@
+#' Visualize the Distribution of Confidence Values per Keypoint
+#'
+#' `check_confidence()` computes summary statistics for confidence values
+#' grouped by a chosen variable (defaulting to `keypoint`) and returns a
+#' tidy data frame that can be visualized with standard plotting functions.
+#'
+#' @param data A data frame that contains at least the columns
+#'   `keypoint` and `confidence`. Additional grouping variables can be
+#'   supplied via the `by` argument.
+#' @param by   (Optional) A character vector or column name(s) used to
+#'   group the data before summarising. If `NULL`, the function defaults to
+#'   `"keypoint"`.
+#' @param ... Arguments passed down to the plotting functions.
+#'
+#' @return A tibble/data frame with one row per group defined by `by`,
+#'   containing the following columns:
+#'   \describe{
+#'     \item{mean_confidence}{Mean of `confidence` within the group (NA‑removed).}
+#'     \item{q1}{First quartile (25th percentile) of `confidence`.}
+#'     \item{q3}{Third quartile (75th percentile) of `confidence`.}
+#'   }
+#'   The result can be passed directly to `ggplot2` or other visualization
+#'   packages.
+#'
+#' @export
+check_confidence <- function(data, ...) {
+  UseMethod("check_confidence")
+}
+
+# default ----------------------------
+
+#' @rdname check_confidence
+#' @export
+check_confidence.default <- function(
+  data,
+  by = NULL,
+  ...
+) {
+  if (!aniframe::is_aniframe(data)) {
+    cli::cli_abort("Data is not an aniframe.")
+  }
+
+  if (is.null(by)) {
+    by <- "keypoint"
+  }
+  summarised_data <- data |>
+    dplyr::ungroup() |>
+    dplyr::summarise(
+      mean_confidence = mean(.data$confidence, na.rm = TRUE),
+      q1 = stats::quantile(.data$confidence, probs = 0.25),
+      q3 = stats::quantile(.data$confidence, probs = 0.75),
+      .by = by
+    ) |>
+    suppressWarnings()
+
+  class(summarised_data) <- c(
+    "check_confidence",
+    "plot_check_confidence",
+    "data.frame"
+  )
+  summarised_data
+}
+
+# methods ----------------------------------
+
+#' @export
+print.check_confidence <- function(x, ...) {
+  rlang::check_installed(
+    "anivis",
+    reason = "to use check_confidence()",
+    action = function(...) {
+      utils::install.packages(
+        'anivis',
+        repos = c(
+          'https://animovement.r-universe.dev',
+          'https://cloud.r-project.org'
+        )
+      )
+    }
+  )
+  NextMethod()
+}
+
+#' @export
+plot.check_confidence <- function(x, ...) {
+  rlang::check_installed(
+    "anivis",
+    reason = "to use check_confidence()",
+    action = function(...) {
+      utils::install.packages(
+        'anivis',
+        repos = c(
+          'https://animovement.r-universe.dev',
+          'https://cloud.r-project.org'
+        )
+      )
+    }
+  )
+  NextMethod()
+}
---
+++ README.md
@@ -0,0 +1,51 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# anicheck
+
+<!-- badges: start -->
+
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.17357778.svg)](https://doi.org/10.5281/zenodo.17357778)
+[![R-CMD-check](https://github.com/animovement/anicheck/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/animovement/anicheck/actions/workflows/R-CMD-check.yaml)
+[![anicheck status
+badge](https://animovement.r-universe.dev/badges/anicheck)](https://animovement.r-universe.dev)
+[![codecov](https://codecov.io/gh/animovement/anicheck/graph/badge.svg?token=Pf5n3yzLzK)](https://codecov.io/gh/animovement/anicheck)
+<!-- badges: end -->
+
+*An R package for diagnosing movement data quality*
+
+The primary aim of the *anicheck* package is to provide diagnostic tools
+for assessing movement data quality, including functions to identify
+missing values, temporal gaps, and spatial outliers.
+
+## Installation
+
+You can install the development version of *anicheck* with:
+
+``` r
+install.packages('anicheck', repos = c('https://animovement.r-universe.dev', 'https://cloud.r-project.org'))
+```
+
+Once you have installed the package, you can load it with:
+
+``` r
+library("anicheck")
+```
+
+## Status
+
+> **Warning**
+>
+> 🏗️ The package is currently in early development and the interface is
+> subject to change. Feel free to play around and provide feedback.
+
+## Citation
+
+If you enjoy the package, please make sure to cite it. If you find a
+bug, feel free to open an issue.
+
+To cite *anicheck* in publications use:
+
+``` r
+citation("anicheck")
+```
